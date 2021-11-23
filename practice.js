7265f825c3a61f2fee290dd2e868a1e2b013cfbb
Btaft6278 <btaft6278@gmail.com>
2021-11-23 16:25:26 -0500

    added more to practice.jy

--- practice.js
+++ practice.js
@@ -1,6 +1,13 @@
 
 
-//FACTORIAL
+//************FACTORIAL*******************//
+
+// Return the factorial of the provided integer.
+// If the integer is represented with the letter n,
+// a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
 //Implement a function named factorial that has one parameter: an integer,n.
 //It must return the value of n! 
 
