afa3d5fad9a23fa910eaecc5f94a9f66731e1ef0
Btaft6278 <btaft6278@gmail.com>
2021-11-25 19:05:37 -0500

    eod

--- try-catch.js
+++ try-catch.js
@@ -49,3 +49,15 @@ function reverseString(s) {
 
     console.log(s);
 }
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    (typeof s !== 'string')
+        ? console.log("s.split is not a function")
+        : (s = s.split('').reverse().join(''));
+
+    console.log(s);
+}
