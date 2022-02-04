2eff52fbadceb6ee6d7cc35f857e1c9146437e60
Btaft6278 <btaft6278@gmail.com>
2022-02-04 10:32:43 -0500

    added for in statements

--- forin.js
+++ forin.js
@@ -1,3 +1,24 @@
+
+//for in statement loops through the properties of an Object:(including arrays)
+//for in with object
+const person = {fname:"John", lname:"Doe", age:25};
+
+let text = "";
+for (let x in person) {
+
+  text = person[x];
+  console.log(text)
+}
+
+//forin with array
+const numbers = [45, 4, 9, 16, 25];
+
+let num = "";
+for(let x in numbers){
+let num = numbers[x];
+console.log(num)
+}
+
 const o = ['first', 'second', false];
 
 // 'p' is the index
