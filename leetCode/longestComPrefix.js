719eebe451f211fce3de4653482e51617afdbcd6
Btaft6278 <btaft6278@gmail.com>
2022-02-27 08:18:07 -0500

    added longest common prefix

--- leetCode/longestComPrefix.js
+++ leetCode/longestComPrefix.js
@@ -1,16 +1,19 @@
-strs = ["flower", "flow", "flight"]
-strs1 = ["flower", "flow", "flight"]
+const strs = ["flower", "flow", "flight"]
 
-var longestCommonPrefix = function (strs) {
+const longestCommonPrefix = (strs) => {
     let prefix = "";
-    for (let i = 0; i < strs[0].length; i++) {
+    if (strs === null || strs.length === 0) return prefix
+    for (i = 0; i < strs[0].length; i++) {
         let first = strs[0][i]
         for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== first) return prefix
+            if (strs[j][i] !== first) {
+                return prefix
+            }
         }
         prefix = prefix + first
+
     }
     return prefix
 }
-
 console.log(longestCommonPrefix(strs))
+//longestCommonPrefix(strs)
