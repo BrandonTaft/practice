86d39310c10e10e8f375d63e21d8c6483ac4c534
Btaft6278 <btaft6278@gmail.com>
2022-02-28 19:59:16 -0500

    added strStr

---
+++ leetCode/needleHaystack.js
@@ -0,0 +1,8 @@
+var strStr = function(haystack, needle) {
+    if(needle == ""){
+    		return 0
+    } if(haystack.includes(needle)){
+    		return haystack.indexOf(needle)
+    }
+    return -1
+};
