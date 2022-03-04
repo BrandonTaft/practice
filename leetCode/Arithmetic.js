21f869d5fcd38343cd30eb68943aa728eddec472
Btaft6278 <btaft6278@gmail.com>
2022-03-03 19:25:04 -0500

    added

--- leetCode/Arithmetic.js
+++ leetCode/Arithmetic.js
@@ -17,3 +17,18 @@ var numberOfArithmeticSlices = function (nums) {
     }
 };
 numberOfArithmeticSlices(nums)
+
+const nums = [1, 2, 3, 4, 5,6];
+
+var numberOfArithmeticSlices = function(nums) {
+    let count = 0, diff, ans = 0
+    for (let i = 1; i < nums.length; i++) {
+        let newDiff = nums[i] - nums[i-1]
+        
+       if (newDiff === diff) ans += count++
+       
+       
+        else diff = newDiff, count = 1
+    }
+   return ans
+};
